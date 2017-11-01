31ab4588a3ed32a472618a9dd85143d3e59d3fba
jx00109 <shu_ytj@163.com>
2017-11-01 20:27:04 +0800

    数据预处理脚本

--- dataCleaner.py
+++ dataCleaner.py
@@ -7,3 +7,22 @@
 输入: *.xlsx
 输出: *.pkl
 '''
+import xlrd
+import Utils
+import re
+
+dataPath = r'./data/Berlinetta MLK 9.06.xlsx'
+
+rawFile = xlrd.open_workbook(dataPath)  # 打开文件
+rawData = rawFile.sheets()[1]  # 打开工作表
+rawDataRows = rawData.nrows  # 9825
+rawDataCols = rawData.ncols  # 47
+count = 0
+for i in range(1, rawDataRows):
+    r = rawData.row_values(i)
+    text = Utils.replaceAllSymbols(r[30])
+    label1, label2, label3 = r[33], r[34], r[35]  # 获取对话内容以及对应的三级分类
+    if Utils.checkOnlyContainEnglish(text): #7244
+        count += 1
+
+print count
