ebf142a6f4dc8372d43a0527095014de808db80c
Hope_5 <shu_ytj@163.com>
2017-11-02 15:18:48 +0800

    对代码进行了修改

--- Utils.py
+++ Utils.py
@@ -2,16 +2,25 @@
 import sys
 import string
 import re
+import xlrd
+import pickle as pkl
 
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-specialsymbols = "[\s+\.\!\/_,$%^*(+\"\'"+string.punctuation+"]+|[+——！，。？<>《》：；、~@#￥%……&*（）]+"
+specialsymbols = "[\s+\.\!\/_,$%^*(+\"\'" + string.punctuation + "]+|[+——！，。？<>《》：；、~@#￥%……&*（）]+"
+mathsysmbols = '\d+(\.\d+)*([×\+\-\*\/]\d+(\.\d+)*)*[0-9A-Za-z]*'
+
+
 # """!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""
 
 # 将所有中英文符号替换成空格
 def replaceAllSymbols(oldStr):
+    # 去掉数字
+    oldStr = re.sub(mathsysmbols.decode("utf-8"), " ".decode("utf-8"), oldStr)
+    # 再去掉符号
     return re.sub(specialsymbols.decode("utf8"), " ".decode("utf8"), oldStr)
+
 
 # 检测是否含有中文
 def checkContainChinese(check_str):
@@ -22,9 +31,30 @@ def checkContainChinese(check_str):
 
 
 def checkOnlyContainEnglish(check_str):
+    if check_str.strip() == '':
+        return False
     for ch in check_str.decode('utf-8'):
         if ch.isalpha() or ch.isdigit() or ch == ' ':
             continue
         else:
             return False
     return True
+
+
+# 打开excel文件
+def openExcel(dataPath, index):
+    rawFile = xlrd.open_workbook(dataPath)  # 打开文件
+    rawData = rawFile.sheets()[index]  # 打开工作表
+    rawDataRows = rawData.nrows  # 9825
+    rawDataCols = rawData.ncols  # 47
+    return rawData, rawDataRows, rawDataCols
+
+
+def saveAsPickle(data, outpath):
+    with open(outpath, 'wb') as f:
+        pkl.dump(data, f)
+
+
+def loadPickle(datapath):
+    with open(datapath, 'rb') as f:
+        return pkl.load(f)
--- dataCleaner.py
+++ dataCleaner.py
@@ -5,24 +5,23 @@
 2. 对原始数据中的标点和不规则单词进行处理
 -------------------------------------------
 输入: *.xlsx
-输出: *.pkl
+输出: *.pkl (list)
 '''
-import xlrd
 import Utils
-import re
 
 dataPath = r'./data/Berlinetta MLK 9.06.xlsx'
+pklDataPath = './data/alldata.pkl'
 
-rawFile = xlrd.open_workbook(dataPath)  # 打开文件
-rawData = rawFile.sheets()[1]  # 打开工作表
-rawDataRows = rawData.nrows  # 9825
-rawDataCols = rawData.ncols  # 47
-count = 0
-for i in range(1, rawDataRows):
+rawData, rawDataRows, rawDataCols = Utils.openExcel(dataPath=dataPath, index=1)  # 9825 rows, 47 cols
+
+data = list()
+
+for i in range(1, rawDataRows):  # 跳过表头
     r = rawData.row_values(i)
     text = Utils.replaceAllSymbols(r[30])
-    label1, label2, label3 = r[33], r[34], r[35]  # 获取对话内容以及对应的三级分类
-    if Utils.checkOnlyContainEnglish(text): #7244
-        count += 1
+    label1, label2, label3 = r[42], r[43], r[44]  # 获取对话内容以及对应的三级分类
+    if Utils.checkOnlyContainEnglish(text):  # 7173 rows, maxlen=572, minlen=2
+        data.append([text.strip().lower(), label1.lower(), label2.lower(), label3.lower()])
 
-print count
+# 保存为pkl文件
+Utils.saveAsPickle(data, pklDataPath)
