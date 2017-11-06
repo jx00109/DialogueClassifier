d4424e262506f1c3f9671f80868df66cf8048f3e
Hope_5 <shu_ytj@163.com>
2017-11-06 15:45:30 +0800

    略作修改

--- dataCleaner.py
+++ dataCleaner.py
@@ -10,17 +10,17 @@
 import Utils
 
 dataPath = r'./data/Berlinetta MLK 9.06.xlsx'
-pklDataPath = './data/alldata.pkl'
+pklDataPath = './data/alldata(onlyEng).pkl'
 
 rawData, rawDataRows, rawDataCols = Utils.openExcel(dataPath=dataPath, index=1)  # 9825 rows, 47 cols
 
 data = list()
 
-for i in range(1, rawDataRows):  # 跳过表头
+for i in range(1, rawDataRows):                         # 跳过表头
     r = rawData.row_values(i)
     text = Utils.replaceAllSymbols(r[30])
-    label1, label2, label3 = r[42], r[43], r[44]  # 获取对话内容以及对应的三级分类
-    if Utils.checkOnlyContainEnglish(text):  # 7173 rows, maxlen=572, minlen=2
+    label1, label2, label3 = r[42], r[43], r[44]        # 获取对话内容以及对应的三级分类
+    if Utils.checkOnlyContainEnglish(text):             # 7173 rows, maxlen=572, minlen=2
         data.append([text.strip().lower(), label1.lower(), label2.lower(), label3.lower()])
 
 # 保存为pkl文件
